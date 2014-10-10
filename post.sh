2b9da9ad1e3cffaa959485c3db9e5b7e8cb27adf
Litvinov Sergey <slitvinov@gmail.com>
2014-10-10 22:03:38 +0200

    Add comments

--- post.sh
+++ post.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# where MCF data files are?
+dname=data
+
+# MCF to lammps
 bash mcf2lammps.sh data/mcf_particles*.out
 
-cd data
-PYTHONPATH=~/Pizza.py/src  python ../dump2ensight.py lmp_dump.*.out
+# lammps to ENSIGHT
+cd "${dname}"
+PYTHONPATH=~/Pizza.py/src python ../dump2ensight.py lmp_dump.*.out
