968fbd968902b717d4b32f7a4a441a55889bd2bd
Litvinov Sergey <slitvinov@gmail.com>
2014-10-10 20:09:49 +0200

    Change a path in post.sh

--- post.sh
+++ post.sh
@@ -3,4 +3,4 @@
 bash mcf2lammps.sh data/mcf_particles*.out
 
 cd data
-PYTHONPATH=~/work/Pizza.py/src  python ../dump2ensight.py lmp_dump.*.out
+PYTHONPATH=~/Pizza.py/src  python ../dump2ensight.py lmp_dump.*.out
